7613542e9ab98177283d67a28157bbd88305966c
HuJyunKai <contact@sivilization.com>
2021-01-15 22:31:32 +0800

    Update start_dockerd.sh

--- start_dockerd.sh
+++ start_dockerd.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
+
+cd /etc/rootless_docker
+#set -x
+storage_path=/etc/rootless_docker/user_data
+mkdir -p $storage_path/$(whoami)/.config/docker/
+
+echo '{
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}' > $storage_path/$(whoami)/.config/docker/daemon.json
+
+cp -rnp bin $storage_path/$(whoami)
+cd $storage_path/$(whoami)/bin
+
+export HOME=$storage_path/$(whoami)
+export XDG_RUNTIME_DIR="$HOME/.docker/run"
+
+export PATH=${storage_path}/$(whoami)/bin:$PATH
+export DOCKER_HOST=unix://${storage_path}/$(whoami)/.docker/run/docker.sock
+
 tmux has-session -t dockerd 2>/dev/null
-
 if [ $? != 0 ]; then
-  cd /etc/rootless_docker
-  #set -x
-  storage_path=/etc/rootless_docker/user_data
-  mkdir -p $storage_path/$(whoami)/.config/docker/
-
-  echo '{
-      "runtimes": {
-          "nvidia": {
-              "path": "nvidia-container-runtime",
-              "runtimeArgs": []
-          }
-      }
-  }' > $storage_path/$(whoami)/.config/docker/daemon.json
-
-  cp -rnp bin $storage_path/$(whoami)
-  cd $storage_path/$(whoami)/bin
-
-  export HOME=$storage_path/$(whoami)
-  export XDG_RUNTIME_DIR="$HOME/.docker/run"
-
   # Running dockerd in background
   tmux new -d -s dockerd ./dockerd-rootless-tmux.sh $storage_path
   #./dockerd-rootless-tmux.sh $storage_path
-
-  export PATH=${storage_path}/$(whoami)/bin:$PATH
-  export DOCKER_HOST=unix://${storage_path}/$(whoami)/.docker/run/docker.sock
-
   { # try
       if [ ! -f ~/.rootless_docker_success ]; then
           sleep 1 &&
