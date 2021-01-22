d04ce2af174546fb6e53a4c4c69ff9ff0f8f4833
HuJyunKai <contact@sivilization.com>
2021-01-22 21:25:32 +0800

    Update install-rootless-docker.sh

--- install-rootless-docker.sh
+++ install-rootless-docker.sh
@@ -5,8 +5,8 @@ grep -qxF "source <( /etc/rootless_docker/start_dockerd.sh )" /etc/skel/.bashrc 
 
 sysctl --system
 
-apt-get install -y uidmap iptables nvidia-container-runtime
-apt-get install tmux newuidmap newgidmap
+apt-get install -y uidmap iptables tmux newuidmap newgidmap 
+apt-get install -y nvidia-container-runtime
 export dv=20.10.2
 mkdir download
 cd download
